a455cbd377831668d51c3b7a8d4a2d25fd0af20c
TOUMI Nassima <toumi.nassi@gmail.com>
2017-02-07 16:19:12 +0200

    Update apacheinstall.sh

--- apacheinstall.sh
+++ apacheinstall.sh
@@ -3,7 +3,7 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
 
 ## Install AMP
-sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y
+sudo apt-get install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 -y
 
 sudo apt-get install libapache2-mod-php5 php5 php5-common php5-curl php5-dev php5-gd php5-idn php-pear php5-imagick php5-mcrypt php5-mysql php5-ps php5-pspell php5-recode php5-xsl -y
 
