4700586913a5dc8fd882fdda8a96a39c595067a1
TOUMI Nassima <toumi.nassi@gmail.com>
2017-02-28 14:47:06 +0200

    Update server_beeswarmdrone.sh

--- server_beeswarmdrone.sh
+++ server_beeswarmdrone.sh
@@ -2,6 +2,6 @@
 sudo -i
 cd /home/ubuntu
 echo "$server_private_floatingIp  beeswarmserver" >> /etc/hosts
-curl -v https://beeswarmserver:5000/login -k -L -d "username=admin&password=ryrzeabbtnpri" -c cookie
-curl -v https://beeswarmserver:5000/ws/drone/add -k -L -b cookie | grep '<pre><code>'| sed 's/<[^>]*>//g'>file
-beeswarm --config $(cat file)
+curl -v https://beeswarmserver:5000/login -k -L -d "username=admin&password=$(cat /home/ubuntu/password)" -c cookie
+curl -v https://beeswarmserver:5000/ws/drone/add -k -L -b cookie | grep '<pre><code>'| sed 's/<[^>]*>//g'>/home/ubuntu/file
+beeswarm --config $(cat /home/ubuntu/file)
